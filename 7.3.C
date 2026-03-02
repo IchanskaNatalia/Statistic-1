f2b7db36d4240333966655644a29d78b2c2757fb
Ichanska Natalia <ichanska.natalia@knu.ua>
2026-03-02 13:10:30 +0200

    .....AAAA

---
+++ 7.3.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#define N 10
+int input_array(double ar[N]){
+    for(int i =0;i<N;i ++){
+        print f("input ar[%d]=",i);
+       int ch scanf("%if",&ar[i]);
+       if(ch!=1) return i;
+
+    }
+    return N;
+}
